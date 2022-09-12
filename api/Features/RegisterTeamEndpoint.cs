2e1507af89e25fbc89603e59561f286eb0ce22f3
Aksel Kvitberg <aksel.kvitberg@outlook.com>
2022-09-12 21:31:53 +0200

    Login with cookie

---
+++ api/Features/RegisterTeamEndpoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Features;
+
+[ApiController]
+[Route("team")]
+public class RegisterTeamEndpoint : ControllerBase
+{
+    private readonly ILogger<RegisterTeamEndpoint> _logger;
+
+    public RegisterTeamEndpoint(ILogger<RegisterTeamEndpoint> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<TeamResult>> RegisterTeam(RegisterTeamEvent registerTeamEvent)
+    {
+        // register team
+
+        var claimsIdentity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, registerTeamEvent.TeamName),
+        }, "qr-code");
+
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+        await Request.HttpContext.SignInAsync("Cookies", claimsPrincipal);
+
+        return new TeamResult(registerTeamEvent.TeamName, registerTeamEvent.TeamMemberCount, registerTeamEvent.GroupCode);
+    }
+
+    [HttpPost("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        return NoContent();
+    }
+}
+
+public class RegisterTeamEvent
+{
+    public string GroupCode { get; set; } = "";
+    public string TeamName { get; set; } = "";
+    public int TeamMemberCount { get; set; }
+}
+
+public record TeamResult(string TeamName, int TeamMemberCount, string GroupName);
--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,33 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+}).AddCookie("Cookies", options =>
+{
+    options.Cookie.Name = "team_cookie";
+    options.Cookie.SameSite = SameSiteMode.None;
+    options.Events = new CookieAuthenticationEvents()
+    {
+        OnRedirectToLogin = recontext =>
+        {
+            recontext.HttpContext.Response.StatusCode = 401;
+            return Task.CompletedTask;
+        },
+    };
+});
+
+builder.Services.AddDataProtection()
+    .SetApplicationName("QR-App")
+    .PersistKeysToAzureBlobStorage(builder.Configuration.GetConnectionString("Storage"), "keys", "dataprotection");
+
+builder.Services.AddCors();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,6 +42,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(policy =>
+{
+    policy.AllowAnyHeader();
+    policy.AllowAnyMethod();
+    //policy.AllowAnyOrigin();
+    policy.AllowCredentials();
+});
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
