412a112ba8428d80c3308bacf8dcdc5f18a37345
rvanoord <reng.vanoord@bcc.no>
2022-09-19 19:34:29 +0200

    Added SQL resources

--- infra/main.tf
+++ infra/main.tf
@@ -32,6 +32,13 @@ provider "azurerm" {
 provider "azapi" {
 }
 
+provider "postgresql" {
+  superuser = false
+  host      = data.azurerm_postgresql_flexible_server.postgresql_server.fqdn
+  username  = "psqladmin"
+  password  = data.azurerm_key_vault_secret.postgresql_admin_password.value
+}
+
 data "azurerm_client_config" "current" {}
 
 locals {
@@ -107,7 +114,9 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
   geo_redundant_backup_enabled = false
   create_mode = "Default"
 
-  tags = local.tags
+  tags                          = local.tags
+
+  zone                          = "1"
 
   # delegated_subnet_id = 
   # private_dns_zone_id =
@@ -124,4 +133,86 @@ resource "azurerm_postgresql_flexible_server_firewall_rule" "terraform_deploy_ip
   server_id        = azurerm_postgresql_flexible_server.postgresql.id
   start_ip_address = "${chomp(data.http.myip.body)}"
   end_ip_address   = "${chomp(data.http.myip.body)}"
+}
+
+# Get Container Registry
+data "azurerm_container_registry" "acr" {
+  name                = "bccplatform"
+  resource_group_name = "BCC-Platform"
+}
+
+# Get Key Vault for postgresql server
+data "azurerm_key_vault" "keyvault" {
+  name                = lower(replace("${local.platform_resource_prefix}-psql","-",""))
+  resource_group_name = local.platform_resource_group
+}
+
+# Get Admin password for postgresql server
+data "azurerm_key_vault_secret" "postgresql_admin_password" {
+  name         = "postgreql-admin-password"
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
+
+# Get Postgresql Server
+data "azurerm_postgresql_flexible_server" "postgresql_server" {
+  name                   = "${local.platform_resource_prefix}-postgresql"
+  resource_group_name    = local.platform_resource_group
+}
+
+# Get platform resource group
+data "azurerm_resource_group" "platform_rg" {
+  name = local.platform_resource_group
+}
+
+# Create Database
+# NB! This will only work if server has a public IP and the client execuring terrafrom has been whitelisted in the server's firewall
+# Perhaps whitelisting the IP address of the current client can be automated...
+module "postgresql_db" {
+  source             = "./modules/azure/postgresql_db"
+  db_name            = local.resource_prefix
+  server_resource_id = data.azurerm_postgresql_flexible_server.postgresql_server.id
+  depends_on = [
+    azurerm_postgresql_flexible_server_firewall_rule.terraform_deploy_ip
+  ]
+}
+
+# Store db user password
+resource "azurerm_key_vault_secret" "postgreql_user_password" {
+  name         = "${local.resource_prefix}-db-user-password"
+  value        = module.postgresql_db.db_user_password
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
+
+# Analytics Workspace
+module "log_analytics_workspace" {
+  source                           = "./modules/azure/log_analytics"
+  name                             = "${local.resource_prefix}-logs"
+  location                         = local.location
+  resource_group_name              = azurerm_resource_group.rg.name
+  tags                             = local.tags
+
+}
+
+# Application Insights
+module "application_insights" {
+  source                           = "./modules/azure/application_insights"
+  name                             = "${local.resource_prefix}-env-insights"
+  location                         = local.location
+  resource_group_name              = azurerm_resource_group.rg.name
+  tags                             = local.tags
+  application_type                 = "web"
+  workspace_id                     = module.log_analytics_workspace.id
+}
+
+# VLAN for Container Environment
+module "container_apps_vlan" {
+  source                           = "./modules/azure/container_apps_vlan"
+  name                             = "${local.resource_prefix}-vlan"
+  location                         = local.location
+  resource_group_name              = azurerm_resource_group.rg.name
+  tags                             = local.tags
+
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
 }
