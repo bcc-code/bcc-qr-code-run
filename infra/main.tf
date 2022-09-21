820423dff287d32abaff847af5e44de0c90d78d5
Reng van Oord <reng.vanoord@bcc.no>
2022-09-21 22:40:26 +0200

    Fixed typo

--- infra/main.tf
+++ infra/main.tf
@@ -407,7 +407,7 @@ module "api_container_app" {
 
 resource "azurerm_storage_account" "file_storage" {
   name                     = local.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.id.name
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
