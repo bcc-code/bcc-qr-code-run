6a9f35b65bea8b91d1bb1609080ba71903b9a2b7
Reng van Oord <reng.vanoord@bcc.no>
2022-09-21 21:03:46 +0200

    Added private DNS zone

--- infra/main.tf
+++ infra/main.tf
@@ -204,9 +204,10 @@ resource "azurerm_redis_cache" "redis_cache" {
 
 # Private DNS zone
 
-data "azurerm_private_dns_zone" "dns_zone" {
+resource "azurerm_private_dns_zone" "redis_dns_zone" {
   name                = "privatelink.redis.cache.windows.net"
   resource_group_name = local.resource_group
+  location            = local.location
 }
 
 # Redis private endpoint
