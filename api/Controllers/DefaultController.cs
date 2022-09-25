3d65a13766c145b9a6f73384fb04f397c72e7284
Reng van Oord <reng.vanoord@bcc.no>
2022-09-25 20:59:49 +0200

    Added healthcheck endpoint

---
+++ api/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [Route("")]
+        [HttpHead]
+        [HttpGet]
+        public ActionResult Ready()
+        {
+            return Ok();
+        }
+    }
+}
