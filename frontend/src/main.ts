414667216b1fed59c11f1878195a293e4fb073bb
Reng van Oord <reng.vanoord@bcc.no>
2022-09-25 21:36:40 +0200

    Application insights take 2

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import VueAppInsights from "vue-application-insights";
 
-
-const app = createApp(App);
-app.use(VueAppInsights, {
-    id: 'dc25eddd-f2aa-4dcc-adaf-52d51c56aeaf'
-});
-app.mount('#app')
+createApp(App).mount('#app')
