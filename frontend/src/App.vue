19477c3c535a53bd874298be984088ad0d4ce4ef
Reng van Oord <reng.vanoord@bcc.no>
2022-09-26 20:01:26 +0200

    Design improvements

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,18 +8,20 @@ getLoggedInTeam()
 </script>
 <template>
 <!--  <TeamPage/>-->
-  <TeamPage v-if="store.isLoggedIn"></TeamPage>
-  <LandingPage v-else/>
-<!--  <div class="container mx-auto">-->
-<!--    <div class="flex flex-col">-->
-<!--      <div class="text-3xl py-5">Hello world</div>-->
-<!--      <div class="flex">-->
-<!--        <button class="bg-interactive rounded text-white font-bold text-lg py-2 px-5" @click="login">Login</button>-->
-<!--        <button class="bg-interactive rounded text-white font-bold text-lg py-2 px-5" @click="get">Get</button>-->
-<!--        <button class="bg-interactive rounded text-white font-bold text-lg py-2 px-5" @click="logout">Logout</button>-->
-<!--      </div>-->
-<!--      <pre>{{reply}}</pre>-->
+  <div>
+    <TeamPage v-if="store.isLoggedIn"></TeamPage>
+    <LandingPage v-else/>
+  <!--  <div class="container mx-auto">-->
+  <!--    <div class="flex flex-col">-->
+  <!--      <div class="text-3xl py-5">Hello world</div>-->
+  <!--      <div class="flex">-->
+  <!--        <button class="bg-interactive rounded text-white font-bold text-lg py-2 px-5" @click="login">Login</button>-->
+  <!--        <button class="bg-interactive rounded text-white font-bold text-lg py-2 px-5" @click="get">Get</button>-->
+  <!--        <button class="bg-interactive rounded text-white font-bold text-lg py-2 px-5" @click="logout">Logout</button>-->
+  <!--      </div>-->
+  <!--      <pre>{{reply}}</pre>-->
 
-<!--    </div>-->
-<!--  </div>-->
+  <!--    </div>-->
+  <!--  </div>-->
+  </div>
 </template>
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -9,12 +9,12 @@ module.exports = {
       
       "white": "#fffceb",
       "light_background": "#fffceb",
-      "background": "#d19a48",
-      "brown": "#423e36",
+      "background": "#d19a48", //yellow
+      "brown": "#423e36", //dark
       "interactive": "#f2ca80",
       "interactive_active": "#e5be79",
-      "accent": "#cb7055",
-      "accept2": "#938b43"
+      "accent": "#cb7055", //Red
+      "accept2": "#938b43" //Green
     },
     extend: {},
   },
