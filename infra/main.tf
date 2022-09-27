741f35eead5c0e9154fc3f940070b585c72428f0
Oz Okun <112382001+Oz-Okun@users.noreply.github.com>
2022-09-27 15:28:06 +0200

    Terraform cloud deployment

--- infra/main.tf
+++ infra/main.tf
@@ -50,10 +50,10 @@ provider "azuread" {
 
 provider "postgresql" {
   superuser = false
-  host      = azurerm_postgresql_flexible_server.postgresql.fqdn
+  host      = data.azurerm_postgresql_flexible_server.postgresql.fqdn
   database  =  local.resource_prefix
   username  = "psqladmin"
-  password  = azurerm_key_vault_secret.postgresql_admin_password.value
+  password  = data.azurerm_key_vault_secret.postgresql_admin_password.value
   sslmode   = "require"
 }
 
