f7cda5db3f6502af9bcf6ca3747e44a5ad1d49e2
Aksel Kvitberg <aksel.kvitberg@outlook.com>
2022-10-02 22:12:27 +0200

    Rengs amazing migration

---
+++ api/Migrations/20221002201033_RengsAmazingMigration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class RengsAmazingMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "Churches",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Participants",
+                table: "Churches",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "CacheEntries",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false),
+                    Expires = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CacheEntries", x => x.Key);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CacheEntries");
+
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "Churches");
+
+            migrationBuilder.DropColumn(
+                name: "Participants",
+                table: "Churches");
+        }
+    }
+}
