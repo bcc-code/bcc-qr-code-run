597f3718d24cdf6db66a2e44cc96400b2e80f14d
Reng van Oord <reng.vanoord@bcc.no>
2022-10-03 23:01:56 +0200

    More styling

--- frontend/src/Pages/LandingPage.vue
+++ frontend/src/Pages/LandingPage.vue
@@ -25,11 +25,13 @@ const showResults = ref(false);
       
       <div class="mx-auto space-y-6 py-6 px-4">
         <h2 class="text-3xl font-bold tracking-tight text-accept2 text-center" id="join-heading"><span class="text-accent">QR-løp:</span> Jorden rundt</h2>
-        <p class="text-lg text-brown">Ta med noen av vennene dine og bli med på en reise <span class="text-accent">jorden rundt</span>!</p>
-        <p> Blir det <i>din</i> menighet som har best oppslutning?</p>
+        <p class="text-lg text-brown">Ta med noen av vennene dine og bli med <b></b>lørdag 8. oktober</b>, på en reise <span class="text-accent">jorden rundt</span>!</p>
+        <p> Blir det <i>din</i> menighet som har <b>best oppslutning</b>?</p>
         <a class="block w-full rounded-md bg-interactive text-xl py-3 px-5 text-center text-base font-medium text-brown shadow-md active:bg-interactive_active active:text-white" @click="showRegister = true">Kom i gang!</a>
-        <h2 class="text-2xl font-bold text-accent tracking-tight">Resultater</h2>
-        <Results/>
+        <div v-if="showResults">
+          <h2 class="text-2xl font-bold text-accent tracking-tight">Resultater</h2>
+          <Results/>
+        </div>
       </div>
     </div>
   </div>
